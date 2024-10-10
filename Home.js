d3807ac6a9a82014df4f5fb89b6b5f5a21ed644b
Karim <karim.ebrahim2003@gmail.com>
2024-10-10 05:32:31 +0300

    adding section 1 at home,adding footer and adding arrow up

---
+++ Home.js
@@ -0,0 +1,32 @@
+    // scrolling up
+let scrolling = document.querySelector(".up");
+window.onscroll = function () {
+  if (this.scrollY >= 900) {
+    scrolling.classList.add("show");
+  } else {
+    scrolling.classList.remove("show");
+  }
+}
+scrolling.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+    // scrolling up ends
+
+    // section1-sliding-image
+var indexValue = 0;
+function slideShow(){
+setTimeout(slideShow, 2500);
+var x;
+const img = document.querySelectorAll(".section1-img");
+for(x = 0; x < img.length; x++){
+img[x].style.display = "none";
+}
+indexValue++;
+if(indexValue > img.length){indexValue = 1}
+img[indexValue -1].style.display = "block";
+}
+slideShow();
+    // section1 ends
